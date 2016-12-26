86c4f4e2a83aac471dd222670d9a41cfdaa931d3
Vladimir E <evdokimov.vladimir@gmail.com>
2016-12-26 18:00:50 +0300

    Blank timber theme (revert to using timber's grid)

--- assets/theme.js
+++ assets/theme.js
@@ -1,2 +1,17 @@
 // Theme functions
 window.theme = window.theme || {};
+
+theme.cacheSelectors = function () {
+  theme.cache = {
+    // General
+    $html : $('html'),
+    $body : $(document.body)
+  };
+};
+
+theme.init = function () {
+  FastClick.attach(document.body);
+  theme.cacheSelectors();
+};
+
+$(theme.init);
