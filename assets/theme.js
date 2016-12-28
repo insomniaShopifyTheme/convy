c65f6ec44ba90e56769d0a9bae1c598affce02a8
Vladimir E <evdokimov.vladimir@gmail.com>
2016-12-28 23:30:39 +0300

    Slideshow section (slick.js)

--- assets/theme.js
+++ assets/theme.js
@@ -14,4 +14,183 @@ theme.init = function () {
   theme.cacheSelectors();
 };
 
+theme.Sections = function Sections() {
+  this.constructors = {};
+  this.instances = [];
+
+  $(document)
+    .on('shopify:section:load', this._onSectionLoad.bind(this))
+    .on('shopify:section:unload', this._onSectionUnload.bind(this))
+    .on('shopify:section:select', this._onSelect.bind(this))
+    .on('shopify:section:deselect', this._onDeselect.bind(this))
+    .on('shopify:block:select', this._onBlockSelect.bind(this))
+    .on('shopify:block:deselect', this._onBlockDeselect.bind(this));
+};
+
+theme.Sections.prototype = _.extend({}, theme.Sections.prototype, {
+  _createInstance: function(container, constructor) {
+    var $container = $(container);
+    var id = $container.attr('data-section-id');
+    var type = $container.attr('data-section-type');
+
+    constructor = constructor || this.constructors[type];
+
+    if (_.isUndefined(constructor)) {
+      return;
+    }
+
+    var instance = _.extend(new constructor(container), {
+      id: id,
+      type: type,
+      container: container
+    });
+
+    this.instances.push(instance);
+  },
+
+  _onSectionLoad: function(evt) {
+    var container = $('[data-section-id]', evt.target)[0];
+    if (container) {
+      this._createInstance(container);
+    }
+  },
+
+  _onSectionUnload: function(evt) {
+    this.instances = _.filter(this.instances, function(instance) {
+      var isEventInstance = (instance.id === evt.detail.sectionId);
+
+      if (isEventInstance) {
+        if (_.isFunction(instance.onUnload)) {
+          instance.onUnload(evt);
+        }
+      }
+
+      return !isEventInstance;
+    });
+  },
+
+  _onSelect: function(evt) {
+    // eslint-disable-next-line no-shadow
+    var instance = _.find(this.instances, function(instance) {
+      return instance.id === evt.detail.sectionId;
+    });
+
+    if (!_.isUndefined(instance) && _.isFunction(instance.onSelect)) {
+      instance.onSelect(evt);
+    }
+  },
+
+  _onDeselect: function(evt) {
+    // eslint-disable-next-line no-shadow
+    var instance = _.find(this.instances, function(instance) {
+      return instance.id === evt.detail.sectionId;
+    });
+
+    if (!_.isUndefined(instance) && _.isFunction(instance.onDeselect)) {
+      instance.onDeselect(evt);
+    }
+  },
+
+  _onBlockSelect: function(evt) {
+    // eslint-disable-next-line no-shadow
+    var instance = _.find(this.instances, function(instance) {
+      return instance.id === evt.detail.sectionId;
+    });
+
+    if (!_.isUndefined(instance) && _.isFunction(instance.onBlockSelect)) {
+      instance.onBlockSelect(evt);
+    }
+  },
+
+  _onBlockDeselect: function(evt) {
+    // eslint-disable-next-line no-shadow
+    var instance = _.find(this.instances, function(instance) {
+      return instance.id === evt.detail.sectionId;
+    });
+
+    if (!_.isUndefined(instance) && _.isFunction(instance.onBlockDeselect)) {
+      instance.onBlockDeselect(evt);
+    }
+  },
+
+  register: function(type, constructor) {
+    this.constructors[type] = constructor;
+
+    $('[data-section-type=' + type + ']').each(function(index, container) {
+      this._createInstance(container, constructor);
+    }.bind(this));
+  }
+});
+
+theme.Slideshow = (function() {
+  this.$slideshow = null;
+  var classes = {
+    wrapper: 'slideshow-wrapper',
+    slideshow: 'slideshow',
+    currentSlide: 'slick-current'
+  };
+
+  function slideshow(el) {
+    this.$slideshow = $(el);
+    this.$wrapper = this.$slideshow.closest('.' + classes.wrapper);
+    this.$pause = this.$wrapper.find('.' + classes.pauseButton);
+
+    this.settings = {
+      accessibility: true,
+      arrows: false,
+      dots: true,
+      fade: true,
+      draggable: true,
+      touchThreshold: 20,
+      autoplay: this.$slideshow.data('autoplay'),
+      autoplaySpeed: this.$slideshow.data('speed')
+    };
+
+    this.$slideshow.slick(this.settings);
+  }
+
+  return slideshow;
+})();
+
+theme.slideshows = {};
+
+theme.SlideshowSection = (function() {
+  function SlideshowSection(container) {
+    var $container = this.$container = $(container);
+    var sectionId = $container.attr('data-section-id');
+    var slideshow = this.slideshow = '#slideshow_' + sectionId;
+    theme.slideshows[slideshow] = new theme.Slideshow(slideshow);
+  }
+
+  return SlideshowSection;
+})();
+
+theme.SlideshowSection.prototype = _.extend({}, theme.SlideshowSection.prototype, {
+  onUnload: function() {
+    delete theme.slideshows[this.slideshow];
+  },
+
+  onBlockSelect: function(evt) {
+    var $slideshow = $(this.slideshow);
+
+    // Ignore the cloned version
+    var $slide = $('.slideshow__slide--' + evt.detail.blockId + ':not(.slick-cloned)');
+    var slideIndex = $slide.data('slick-index');
+
+    // Go to selected slide, pause autoplay
+    $slideshow.slick('slickGoTo', slideIndex).slick('slickPause');
+  },
+
+  onBlockDeselect: function() {
+    // Resume autoplay
+    $(this.slideshow).slick('slickPlay');
+  }
+});
+
+$(document).ready(function() {
+  var sections = new theme.Sections();
+
+  sections.register('slideshow-section', theme.SlideshowSection);
+});
+
 $(theme.init);
