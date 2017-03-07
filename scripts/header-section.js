adcfc4e891b41e5f397374307bcf4d3a99dfd31c
Vladimir E <evdokimov.vladimir@gmail.com>
2017-03-07 03:28:28 +0300

    Desktop Header: Search form and My account; closes #50

--- scripts/header-section.js
+++ scripts/header-section.js
@@ -4,12 +4,35 @@
 
 theme.HeaderSection = (function() {
   function HeaderSection(container) {
-    var $container = this.$container = $(container);
-    $('ul.sf-menu').superfish({
+    var $container;
+    this.$container = $container = this.$container = $(container);
+    $container.find('ul.sf-menu').superfish({
       delay: 500,
       speed: 200,
     });
+    $container.find('.js-expand-search').on('click', this._toggleSearchField.bind(this));
+    var _self = this;
+    $container.find('.header-search').on('keyup', function(e) {
+      if (e.key === "Escape") {
+        _self._toggleSearchField();
+        console.log('close');
+      }
+    });
   }
+
+  HeaderSection.prototype = _.extend({}, HeaderSection.prototype, {
+
+    _toggleSearchField: function () {
+      $search = this.$container.find(".header-search");
+      if ($search.hasClass('header-search--active')) {
+        $search.removeClass('header-search--active');
+      } else {
+        $search.addClass('header-search--active');
+        $search.find('input').focus();
+      }
+    }
+
+  });
 
   return HeaderSection;
 })();
