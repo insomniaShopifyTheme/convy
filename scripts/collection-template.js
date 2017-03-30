13101a0be50d70e22e852eee6d3b73b6e28e73a6
Vladimir E <evdokimov.vladimir@gmail.com>
2017-03-30 18:41:34 +0300

    Fix tags filtering; Comma separated color swatches option; re #34 re #73

--- scripts/collection-template.js
+++ scripts/collection-template.js
@@ -6,7 +6,18 @@ theme.CollectionTemplate = (function() {
   function CollectionTemplate(container) {
     var $container = $(container);
     theme.productCardsInit($container);
+
+    $container.find('.collection-filter__items--tags input').change(this.filterUpdate.bind(this));
   }
 
   return CollectionTemplate;
 })();
+
+theme.CollectionTemplate.prototype = _.extend({}, theme.CollectionTemplate.prototype, {
+
+  filterUpdate: function(e) {
+    var $filterLink = $(e.target).parent().find('a');
+    window.location = $filterLink.prop('href');
+  },
+
+});
