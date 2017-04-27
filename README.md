bc6dcc3eb5cc7eaf9ae9ebfb91f4e2c0e4bb3980
Bogdan Radu <bogdanrn@gmail.com>
2017-04-27 16:00:59 +0300

    added gulp tasks

--- README.md
+++ README.md
@@ -3,33 +3,39 @@ Install
 
 Make sure `config.yml` file is setup properly. [Docs here](http://shopify.github.io/themekit/configuration/).
 
-#### 1. Install gulp
+#### 1. Install gulp and dependecies
 ```
 npm install
 ```
 
 ### Development
 
-#### 2. Run gulp watch
+#### 2. Run gulp with default task (builds all files and then watches for changes in scripts and css) 
 ```
-gulp watch
+gulp
 ```
 
 #### 3. Run Shopify theme kit
 ```
-theme watch
+cd ./dist && theme watch
 ```
 
 ### Release
 
-#### 4. Build `theme.js` and `theme.scss` files
+#### 4. Gulp tasks
 ```
-gulp styles
-gulp scripts
+gulp  //builds theme and starts watching
+gulp build //just builds the theme, no watching for changes
+gulp styles       //builds scss files => ./build/assets/theme.css.liquid
+gulp scripts      //builds scripts => ./build/assets/theme.js.liquid
+gulp sync_files   //copy rest of the files to ./build
+gulp zip //builds theme && zips it
 ```
 
 #### 5. Upload everything to shopify
 ```
+gulp build
+cd ./dist
 theme replace
 ```
 
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ var cssimport = require("gulp-cssimport");
 var concat = require('gulp-concat');
 var pump = require('pump');
 var gulpSequence = require('gulp-sequence').use(gulp)
+var del = require('del');
+var changed = require('gulp-changed');
+var zip = require('gulp-zip');
+
 
 var globalConfig = {
   scss_src: './src/scss', // your dev stylesheet directory. No trailing slash
@@ -14,7 +18,7 @@ var globalConfig = {
 gulp.task('styles', function() {
   return gulp.src(globalConfig.scss_src + '/**/*.*')
     .pipe(cssimport())
-    .pipe(gulp.dest('assets/'));
+    .pipe(gulp.dest('./dist/assets/'));
 })
 
 gulp.task('scripts', function() {
@@ -42,7 +46,28 @@ gulp.task('scripts', function() {
       globalConfig.scripts_src + '/init.js',
     ])
     .pipe(concat('theme.js.liquid'))
-    .pipe(gulp.dest('./build/assets/'));
+    .pipe(gulp.dest('./dist/assets/'));
+});
+
+//clean task
+gulp.task('clean', function() {
+  return del([
+    "./dist/**/*",
+  ]);
+});
+
+
+//copy files
+gulp.task('sync_files', function() {
+  return gulp.src([
+      "src/**/*",
+      "!src/scripts/**/*",
+      "!src/scss/**/*",
+      "!src/scripts/",
+      "!src/scss/"
+    ])
+    .pipe(changed("./dist/"))
+    .pipe(gulp.dest('./dist/'));
 });
 
 
@@ -50,11 +75,27 @@ gulp.task('scripts', function() {
 gulp.task('watch', function() {
   gulp.watch(globalConfig.scss_src + '/**/*.*', ['styles']);
   gulp.watch(globalConfig.scripts_src + '/scripts/**/*.js', ['scripts']);
+  gulp.watch([
+    "src/**/*",
+    "!src/scripts/**/*",
+    "!src/scss/**/*",
+    "!src/scripts/",
+    "!src/scss/"
+  ], ["sync_files"]);
 });
 
 
 //build 
-gulp.task('build', gulpSequence('styles', 'scripts'));
+gulp.task('build', gulpSequence('clean', 'sync_files', 'styles', 'scripts'));
 
 // Default task
-gulp.task('default', ['watch']);
+gulp.task('default', gulpSequence('build', 'watch'));
+
+
+gulp.task("zip_build", function() {
+  return gulp.src('./dist/**/*')
+    .pipe(zip('konversion-theme.zip'))
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('zip', gulpSequence('build', 'zip_build'));
