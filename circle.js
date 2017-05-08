6de76c6c9f178b63c23a4248445cf9ee315a8277
Bogdan Radu <bogdanrn@gmail.com>
2017-05-08 19:04:54 +0300

    fixed dl link

--- circle.js
+++ circle.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 const bot = require("circle-github-bot").create();
+
+
 console.log(`
-<h3>${bot.env.commitMessage}</h3>
-Download theme: <strong>${bot.artifactLink('konversion-theme.zip', 'konversion-theme.zip')}</strong>
+Built for: ${bot.env.commitMessage} \n
+Download theme: ${bot.artifactLink('dist/konversion-theme.zip', 'konversion-theme.zip')}
 `);
 bot.comment(`
-<h3>${bot.env.commitMessage}</h3>
-Download theme: <strong>${bot.artifactLink('konversion-theme.zip', 'konversion-theme.zip')}</strong>
+<h4>${bot.env.commitMessage}</h4>
+Download theme: <strong>${bot.artifactLink('dist/konversion-theme.zip', 'konversion-theme.zip')}</strong>
 `);
+
+//https://27-81215894-gh.circle-artifacts.com/0/tmp/circle-artifacts.llsqx1o/konversion-theme.zip
+//https://27-81215894-gh.circle-artifacts.com/0/home/ubuntu/theme_konversion/konversion-theme.zip
