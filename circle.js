ad910f95ed573449c70093fc46ed5f0b3735a94f
Bogdan Radu <bogdanrn@gmail.com>
2017-05-08 18:48:03 +0300

    test

--- circle.js
+++ circle.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const bot = require("circle-github-bot").create();
-
+console.log(`
+<h3>${bot.env.commitMessage}</h3>
+Download theme: <strong>${bot.artifactLink('konversion-theme.zip', 'konversion-theme.zip')}</strong>
+`);
 bot.comment(`
 <h3>${bot.env.commitMessage}</h3>
 Download theme: <strong>${bot.artifactLink('konversion-theme.zip', 'konversion-theme.zip')}</strong>
